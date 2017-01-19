701e9419a698485c1d0b2fee92ea1d92d618b80f
Vi Vo <vi.uyen.vo@gmail.com>
2017-01-19 15:28:52 -0800

    adjust skill bar colors

--- app.js
+++ app.js
@@ -32,10 +32,8 @@ $(document).ready(function(){
       var window_height = $(window).height();
       var window_top_position = $(window).scrollTop();
       var window_bottom_position = (window_top_position + window_height);
-      console.log($.each);
 
       $('.skillbar').each(function() {
-        console.log('hi');
         var $element = $(this);
         var element_height = $element.outerHeight();
         var element_top_position = $element.offset().top;
